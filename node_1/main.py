763ab5cdc045b8e221cfd8d2cdeb9a7e5ea2def1
ATONU GHOSH <atonughosh@outlook.com>
2024-11-08 22:38:27 +0530

    Update main.py

--- node_1/main.py
+++ node_1/main.py
@@ -1,15 +1,17 @@
-#this is version number 12
-#testing the code resilience on dual core
+#this is version number 2
+#Do NOT update following lines
 NODE_ID = 1
+firmware_url = "https://github.com/atonughosh/tsdpl_vib_temp"
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
+#Update below this
 
 import uasyncio as asyncio
 from machine import Pin
 import time
 import gc
 
-firmware_url = "https://github.com/atonughosh/tsdpl_vib_temp"
+
 
 # Set up pin for the LED
 led = Pin(2, Pin.OUT)  # Most ESP32 boards have an onboard LED on GPIO 2
@@ -25,9 +27,9 @@ async def task1():
 async def task2():
     while True:
         led.value(1)  # Turn on the LED
-        time.sleep(0.5)  # Delay for 500ms
+        time.sleep(2)  # Delay for 500ms
         led.value(0)  # Turn off the LED
-        time.sleep(0.5)  # Delay for 500ms
+        time.sleep(2)  # Delay for 500ms
         await asyncio.sleep(2)
 
 async def main():
