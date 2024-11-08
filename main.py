ee2acf91b6fcf6e3ea5a45ff5979a9a057e39616
ATONU GHOSH <atonughosh@outlook.com>
2024-11-08 20:24:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
+import _thread
 from machine import Pin
 import time
 
 
 firmware_url = "https://github.com/atonughosh/tsdpl_vib_temp"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-
-ota_updater.download_and_install_update_if_available()
 
 # Set up pin for the LED
 led = Pin(2, Pin.OUT)  # Most ESP32 boards have an onboard LED on GPIO 2
 
-# Blinking loop
-while True:
-    led.value(1)  # Turn on the LED
-    time.sleep(2)  # Delay for 500ms
-    led.value(0)  # Turn off the LED
-    time.sleep(2)  # Delay for 500ms
 
-#This is the file from GitHub
+def task1():
+    while True:
+        ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+        ota_updater.download_and_install_update_if_available()
+
+def task2():
+    # Blinking loop
+    while True:
+        led.value(1)  # Turn on the LED
+        time.sleep(0.5)  # Delay for 500ms
+        led.value(0)  # Turn off the LED
+        time.sleep(0.5)  # Delay for 500ms
+
+# Start Task 1 in a new thread
+_thread.start_new_thread(task1, ())
+
+# Run Task 2 in the main thread
+task2()
