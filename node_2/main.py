b6508986bc935787c3b5482452f9b8667ddcb30a
ATONU GHOSH <atonughosh@outlook.com>
2024-11-10 09:30:20 +0530

    Update main.py

--- node_2/main.py
+++ node_2/main.py
@@ -1,7 +1,7 @@
 # main.py
 import machine
 machine.freq(240000000)
-# This is version number 24
+# This is version number 1
 # Do NOT update following lines
 NODE_ID = 2
 firmware_url = "https://github.com/atonughosh/tsdpl_vib_temp"
