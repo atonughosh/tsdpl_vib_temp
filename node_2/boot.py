7ddc67e5de7371ea14acdc2ad962fe0a27cfa44c
ATONU GHOSH <atonughosh@outlook.com>
2024-11-21 07:11:01 +0530

    Create boot.py

---
+++ node_2/boot.py
@@ -0,0 +1,49 @@
+
+import os
+import gc
+import uasyncio as asyncio
+
+# Import SSID and PASSWORD from WIFI_CONFIG.py
+try:
+    from WIFI_CONFIG import SSID, PASSWORD
+except ImportError:
+    print("Error: WIFI_CONFIG.py not found!")
+    SSID = ""
+    PASSWORD = ""
+
+# Your boot actions: 
+
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+import gc
+NODE_ID = "2"
+
+gc.collect()
+firmware_url = "https://github.com/atonughosh/tsdpl_vib_temp"
+
+async def boot_time_ota():
+    try:
+        ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py", NODE_ID)
+        
+        # Check if an update is available
+        await asyncio.sleep(20)
+        if await ota_updater.check_for_updates():
+            # If there's a new version, perform the update
+            ota_updater.update_and_reset()
+        else:
+            print("No update available.")
+    except:
+        print(f"Error during OTA update")
+
+
+# Start the async update process
+async def boot():
+    gc.collect()
+    await boot_time_ota()  # Await the update process
+
+# Execute the boot process using uasyncio
+asyncio.run(boot())
+
+gc.collect()
+
+# You can add more boot actions here if needed
