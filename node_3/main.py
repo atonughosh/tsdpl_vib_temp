21196eb2e55f37a0771d6d264e60c9690125fdf4
ATONU GHOSH <atonughosh@outlook.com>
2024-12-22 10:05:49 +0530

    Update main.py

--- node_3/main.py
+++ node_3/main.py
@@ -21,12 +21,16 @@ BROKER = "13.232.192.17"  # MQTT broker
 PORT = 1883  # MQTT port
 REBOOT_TOPIC = "remote_control"  # Topic for receiving commands
 ##########################Update This########################
-NODE_ID = 2
+NODE_ID = 3
 #############################################################
 TOPIC = f"OC7/data/N{NODE_ID}"  # Topic for publishing
 firmware_url = "https://github.com/atonughosh/tsdpl_vib_temp"
-SSID = "AirFiber-Shoo6u"
-PASSWORD = "phoPee5johx7feuh"
+
+# SSID = "AirFiber-Shoo6u"
+# PASSWORD = "phoPee5johx7feuh"
+
+SSID: "OC7"
+Password: "oc7@bara"
 
 # SPI and I2C setup
 sck = machine.Pin(18, machine.Pin.OUT)
